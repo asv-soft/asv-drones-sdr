1dc6bdb5fbab51ab16014960d29c0bb5151abfe3
Alexey <248997@gmail.com>
2023-09-13 15:14:45 +0500

    feat: build bug fix

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ rem install tool for update project version by git describe
 rem dotnet tool install -g dotnet-setversion
 
 rem copy version to text file, then in variable
-git describe --tags --abbrev=4 >./version.txt
+git describe --tags --abbrev=4 >version.txt
 SET /p VERSION=<version.txt
 DEL version.txt
 SET VERSION=%VERSION:v=%
@@ -18,10 +18,13 @@ rem build all projects
   	echo %%p
   	echo %VERSION%
 	dotnet restore ./src/%%p/%%p.csproj
-	dotnet build /p:SolutionDir=../;ProductVersion=%VERSION:v=% ./src/%%p/%%p.csproj -c Release
-	dotnet publish /p:SolutionDir=../;ProductVersion=%VERSION:v=% ./src/%%p/%%p.csproj -c Release -r linux-arm --self-contained -p:PublishSingleFile=true -f net7.0 -o bin/publish/net6.0-linux-arm
-	dotnet publish /p:SolutionDir=../;ProductVersion=%VERSION:v=% ./src/%%p/%%p.csproj -c Release -r linux-x64 --self-contained -p:PublishSingleFile=true -f net7.0 -o bin/publish/net6.0-linux-x64
-	dotnet publish /p:SolutionDir=../;ProductVersion=%VERSION:v=% ./src/%%p/%%p.csproj -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -f net7.0 -o bin/publish/net6.0-win-x64
+	dotnet build /p:SolutionDir=../;ProductVersion=%VERSION% ./src/%%p/%%p.csproj -c Release
+	dotnet publish /p:SolutionDir=../;ProductVersion=%VERSION% ./src/%%p/%%p.csproj -c Release -r linux-arm --self-contained -p:PublishSingleFile=true -f net7.0 -o bin/publish/linux-arm
+	echo "%VERSION%">>bin/publish/linux-arm/%%p.version
+	dotnet publish /p:SolutionDir=../;ProductVersion=%VERSION% ./src/%%p/%%p.csproj -c Release -r linux-x64 --self-contained -p:PublishSingleFile=true -f net7.0 -o bin/publish/linux-x64
+	echo "%VERSION%">>bin/publish/linux-x64/%%p.version
+	dotnet publish /p:SolutionDir=../;ProductVersion=%VERSION% ./src/%%p/%%p.csproj -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -f net7.0 -o bin/publish/win-x64
+	echo "%VERSION%">>bin/publish/win-x64/%%p.version
 )) 
 
 
